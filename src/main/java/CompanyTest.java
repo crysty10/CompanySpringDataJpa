65219a6d07f0586204268c5a834e62b701048c44
Cojocaru Florin-Alexandru <alexandru.cojocaru11@gmail.com>
2015-02-17 15:26:11 +0200

    add new functionality to addressRepo

--- src/main/java/CompanyTest.java
+++ src/main/java/CompanyTest.java
@@ -11,8 +11,6 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import repository.AddressRepository;
 import repository.EmployeeRepository;
 import javax.inject.Inject;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 
@@ -32,12 +30,10 @@ public class CompanyTest {
     @Before
     public void setUp() throws Exception {
 
-        /*Employee emp = new Employee("Bogdan", "I.", 1500);
-        empRepo.save(emp);
-        Employee emp2 = new Employee("Alex", "C.", 1500);
-        empRepo.save(emp2);
+        //Employee emp = new Employee("Cristian", "D.", 1500);
         Address adress = new Address("Florilor",2,"D12",1,45,"Turnu-Magurele","Romania");
-        addressRepo.save(adress);*/
+        //empRepo.save(emp);
+        addressRepo.save(adress);
     }
 
     @Test
@@ -54,19 +50,5 @@ public class CompanyTest {
         Address adress = addressRepo.findBylocality("Turnu-Magurele");
         assertEquals("Wrong locality", adress.getLocality(), "Turnu-Magurele");
         //addressRepo.delete(adress);
-    }
-
-    @Test
-    public void testFindAllEmployeesBySalary() {
-        List<Employee> employeeList = empRepo.findAllEmployeesBySalary(1500);
-        for(Employee employee : employeeList) {
-            System.out.println("Employee name: " + employee.getFirstname());
-        }
-    }
-
-    @Test
-    public void testFindEmployeeByLastnameWithQuery() {
-        Employee employee = empRepo.findEmployeeByLastname("D.");
-        assertEquals("You didn't get the wright employee!", employee.getFirstname(), "Cristian");
     }
 }
--- src/main/java/repository/AddressRepository.java
+++ src/main/java/repository/AddressRepository.java
@@ -2,7 +2,11 @@ package repository;
 
 import domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Florin.Cojocaru on 2/17/2015.
@@ -12,4 +16,8 @@ import org.springframework.stereotype.Repository;
 public interface AddressRepository extends JpaRepository<Address, Long>  {
 
     Address findBylocality(String locality);
+    List<Address> findByStreet(String street);
+
+    @Query("SELECT A FROM Address A WHERE A.country = :country")
+    List<Address> findAllByCountry(@Param("country") String country);
 }
